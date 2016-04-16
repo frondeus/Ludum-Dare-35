17313b3bf8b6c16d4336e2c4e03da4fb68d541df
Frondeus <frondeus@gmail.com>
2016-04-16 16:02:35 +0200

    Poczatki przeszkod

--- main.js
+++ main.js
@@ -1,46 +1,65 @@
 (function() {
+    var random = function(min, max) {
+        return Math.random() * (max - min) + min;
+    };
+
+    var Obstacle = function(x, y) {
+        this.gfx = new PIXI.Graphics();
+        this.gfx.beginFill(0xFFFFFF);
+        this.gfx.drawCircle(0, 0, 30);
+        this.gfx.endFill();
+
+        this.pos = new Game.Vec(0,0);
+
+        this.pos.x = this.gfx.position.x = x;
+        this.pos.y = this.gfx.position.y = y;
+    };
+
     Game.Width = window.innerWidth;
     Game.Height = window.innerHeight;
 
-    var renderer = new PIXI.WebGLRenderer(Game.Width, Game.Height);
-    renderer.autoResize = true;
-    renderer.resize(Game.Width, Game.Height);
-    document.body.appendChild(renderer.view);
+    Game.init = function() {
+        this.renderer = new PIXI.WebGLRenderer(this.Width, this.Height);
+        this.renderer.autoResize = true;
+        this.renderer.resize(this.Width, this.Height);
+        document.body.appendChild(this.renderer.view);
 
-    var stage = new PIXI.Container();
-    var birds = [];
+        this.stage = new PIXI.Container();
+        this.birds = [];
+        this.mouse = new Game.Vec(100, 100);
 
-    var addBird = function(x, y) {
-        var bird = new Game.Bird(x, y);
-        birds.push(bird);
-        stage.addChild(bird.gfx);
+        for(var i = 0; i < 100; i++) this.addBird(random(100, this.Width - 100), random(100, this.Height - 100));
+
+        this.addObject(Obstacle, random(100, this.Width - 100), random(100, this.Height - 100));
     };
 
-    var randCircle = function(size) {
-        var x = Math.random() * size - (2 * size);
-        return x;
+    Game.addObject = function(Obj, x, y) {
+        var obj = new Obj(x, y);
+        this.stage.addChild(obj.gfx);
+        return obj;
     };
 
-    for(var i = 0; i < 100; i++) addBird(randCircle(1000), randCircle(1000));
+    Game.addBird = function(x, y) {
+        this.birds.push(this.addObject(this.Bird, x, y));
+    };
 
-    var mouse = new Game.Vec(100, 100);
+    Game.animate = function() {
+        this.renderer.render(this.stage);
 
-    console.log(renderer);
+        
+        this.mouse.x = this.renderer.plugins.interaction.mouse.global.x;
+        this.mouse.y = this.renderer.plugins.interaction.mouse.global.y;
 
+        for(var b in this.birds) {
+            this.birds[b].step(this.birds, this.mouse);
+        }
+    };
+
+    Game.init();
     animate();
 
     function animate() {
-        renderer.render(stage);
-
-        
-        mouse.x = renderer.plugins.interaction.mouse.global.x;
-        mouse.y = renderer.plugins.interaction.mouse.global.y;
-
-        for(var b in birds) {
-            birds[b].step(birds, mouse);
-        }
-
-
+        Game.animate();
         requestAnimationFrame(animate);
     }
 
